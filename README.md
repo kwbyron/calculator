43e1da69ab549262fcab5559cfeeb03e01949613
kwbyron <kbyron11@gmail.com>
2023-11-10 12:25:49 -0800

    add layout theme 1

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# Frontend Mentor - QR code component solution
+# Frontend Mentor - Calculator app solution
 
-This is a solution to the [QR code component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/qr-code-component-iux_sIO_H). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
 
 ## Table of contents
 
 - [Overview](#overview)
+  - [The challenge](#the-challenge)
   - [Screenshot](#screenshot)
   - [Links](#links)
 - [My process](#my-process)
@@ -18,6 +19,15 @@ This is a solution to the [QR code component challenge on Frontend Mentor](https
 **Note: Delete this note and update the table of contents based on what sections you keep.**
 
 ## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See the size of the elements adjust based on their device's screen size
+- Perform mathmatical operations like addition, subtraction, multiplication, and division
+- Adjust the color theme based on their preference
+- **Bonus**: Have their initial theme preference checked using `prefers-color-scheme` and have any additional changes saved in the browser
 
 ### Screenshot
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,110 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-
   return (
     <>
-      <h1>Hello!</h1>
+      <Main>
+        <Header>
+          <Toggle />
+        </Header>
+        <Output />
+        <Inputs />
+      </Main>
     </>
-  )
+  );
 }
 
-export default App
+function Main({ children }) {
+  return <div className="main">{children}</div>;
+}
+
+function Header({ children }) {
+  return (
+    <div className="header">
+      <p>calc</p>
+      {children}
+    </div>
+  );
+}
+
+function Output() {
+  return (
+    <div className="output">
+      <p>123,456</p>
+    </div>
+  );
+}
+
+function Inputs() {
+  return (
+    <div className="inputs">
+      <button className="btn number" id="seven">
+        7
+      </button>
+      <button className="btn number" id="eight">
+        8
+      </button>
+      <button className="btn number" id="nine">
+        9
+      </button>
+      <button className="btn" id="delete">
+        DEL
+      </button>
+      <button className="btn number" id="four">
+        4
+      </button>
+      <button className="btn number" id="five">
+        5
+      </button>
+      <button className="btn number" id="six">
+        6
+      </button>
+      <button className="btn operation" id="plus">
+        +
+      </button>
+      <button className="btn number" id="one">
+        1
+      </button>
+      <button className="btn number" id="two">
+        2
+      </button>
+      <button className="btn number" id="three">
+        3
+      </button>
+      <button className="btn operation" id="minus">
+        -
+      </button>
+      <button className="btn operation" id="dot">
+        .
+      </button>
+      <button className="btn number" id="zero">
+        0
+      </button>
+      <button className="btn operation" id="divide">
+        /
+      </button>
+      <button className="btn operation" id="multipe">
+        x
+      </button>
+      <button className="btn" id="reset">
+        RESET
+      </button>
+      <button className="btn" id="equal">
+        =
+      </button>
+    </div>
+  );
+}
+
+function Toggle() {
+  return (
+    <div className="toggle">
+      <p>THEME</p>
+      <div className="slider">
+        <p>1 2 3</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
